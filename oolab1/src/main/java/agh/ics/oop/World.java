0b2fc589dc3522534e1687f7cdc285f991ebd42a
dmarek03 <domarek@student.agh.edu.pl>
2023-10-08 22:56:09 +0200

    Added class World

---
+++ oolab1/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,8 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("System start");
+        System.out.println("System stop");
+    }
+}
